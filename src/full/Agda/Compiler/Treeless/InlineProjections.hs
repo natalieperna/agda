efc0ec1860ca74e8855d0f7eeed98af7b9674b55
Natalie Perna <nmperna@gmail.com>
2016-07-05 19:45:38 -0400

    Simple case over TTerm

--- src/full/Agda/Compiler/Treeless/InlineProjections.hs
+++ src/full/Agda/Compiler/Treeless/InlineProjections.hs
@@ -4,4 +4,9 @@ import Agda.Syntax.Treeless
 import Agda.TypeChecking.Monad
 
 inlineProjections :: TTerm -> TCM TTerm
-inlineProjections body = return body
+inlineProjections body =
+  case body of
+    TCase sc t def alts -> return body
+    -- | TCase Int CaseType TTerm [TAlt]
+    -- ^ Case scrutinee (always variable), case type, default value, alternatives
+    otherwise -> return body
