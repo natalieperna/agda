92f71a6bffbf42a9dee9c84f2cbc24878de52705
Wolfram Kahl <kahl@cas.mcmaster.ca>
2017-05-10 16:17:26 -0400

    Added unfinished Agda.Compiler.Treeless.FloatPatterns.
    In Agda.Compiler.Treeless.NVTTerm, switched fvarsNVTTerm return type
    to VarSet, and sidelined FloatPLetNV.

---
+++ src/full/Agda/Compiler/Treeless/FloatPatterns.hs
@@ -0,0 +1,266 @@
+{-# LANGUAGE CPP #-}
+module Agda.Compiler.Treeless.FloatPatterns where
+
+import Prelude hiding (Floating)
+
+import Control.Applicative
+import Control.Monad.Reader
+import Data.Monoid
+import qualified Data.Map as Map
+import Data.Function (on)
+import Data.Maybe (isJust)
+import Data.List (nub, partition)
+import qualified Data.IntSet as IntSet
+import Data.IntSet (IntSet)
+import qualified Data.IntMap as IntMap
+import Data.IntMap (IntMap)
+
+import Agda.Syntax.Common
+import Agda.Syntax.Treeless as T hiding (PLet(..))
+import Agda.Syntax.Literal
+import Agda.Syntax.Position
+import Agda.Syntax.Fixity
+import Agda.Syntax.Abstract.Name
+import qualified Agda.Syntax.Concrete.Name as C
+import Agda.TypeChecking.Monad
+import Agda.TypeChecking.Substitute
+import Agda.Compiler.Treeless.Subst
+import Agda.Compiler.Treeless.Compare
+import Agda.Compiler.Treeless.NVTTerm
+
+import Agda.Utils.Permutation
+
+import Data.Word (Word64)
+
+import Agda.Utils.Impossible
+#include "undefined.h"
+
+data Floating
+  = FloatingPLet
+    { pletPat :: NVPat
+    , pletRHS :: NVTTerm
+    , pletFVars :: VarSet -- free variables of pletRHS \edcomm{WK}{still used?}
+    }
+  | FloatingCase
+    { fcaseScrutinee :: Var
+    , fcasePat :: NVConPat
+    }
+   deriving (Show)
+
+flFreeVars :: Floating -> VarSet
+flFreeVars plet@(FloatingPLet {}) = pletFVars plet
+flFreeVars (FloatingCase v _) = singletonVarSet v
+
+attachConPatToFloating :: Var -> NVConPat -> Floating -> Maybe Floating
+attachConPatToFloating v conPat plet@(FloatingPLet {})
+  = case attachNVConPat v conPat (pletPat plet) of
+      Nothing -> Nothing
+      Just pat' -> Just plet { pletPat = pat' }
+attachConPatToFloating v conPat fcase@(FloatingCase {})
+  = case attachToNVConPat v conPat (fcasePat fcase) of
+      Nothing -> Nothing
+      Just pat' -> Just fcase { fcasePat = pat' }
+
+applyFloating :: Floating -> NVTTerm -> NVTTerm
+applyFloating plet@(FloatingPLet {pletPat = p}) b {- NVTLet v t1 b) a = -}
+    = NVTLet v (pletRHS plet)
+    $ maybe b (caseNVConPat v `flip` b) (innerNVPat p)
+  where
+    v = getNVPatVar p
+applyFloating fcase@(FloatingCase v p) b = caseNVConPat v p b
+
+{-
+
+-- If |splitPLet t = Just (fl, t')|, then |applyFloating fl t' = t|.
+splitFloating :: NVTTerm -> Maybe (Floating, NVTTerm)
+splitFloating (NVTLet v t1 t2) = case h (NVPVar v) t2 of
+   (pat, t')  -> Just
+      (FloatingPLet
+       { pletPat = pat
+        , pletRHS = t1
+        , pletFVars = fvarsNVTTerm t1
+        }
+      , t')
+   where
+     h :: NVPat -> NVTTerm -> (NVPat, NVTTerm)
+     h p t = case splitSingleAltCase t of
+       Nothing -> (p, t)
+       Just (cv, conPat, b)
+         | Just p' <- attachNVConPat cv conPat p
+         -> h p' b
+splitFloating t = case splitSingleAltCase t of
+       Nothing -> (p, t)
+       Just (cv, conPat, b) ->
+         (FloatingCase
+           { fcaseScrutinee = cv
+           , fcasePat = conPat
+           }
+         , b
+         )
+
+-- |splitSingleAltCase vs t = Just ((v, conPat), t1)| means
+-- that |caseNVConPat v conPat t1 == t|
+-- and |vs'| contains |vs| and the additional pattern variables of |t0|
+splitSingleAltCase :: [Var] -> NVTTerm -> Maybe (Var, NVConPat, NVTTerm)
+splitSingleAltCase vs (NVTCase v ct dft [NVTACon c cvars t2]) | harmless dft
+  = let ((b', vs'), t') = let vs2 = vs ++ cvars in case splitBoundedCase vs2 t2 of
+          Nothing -> ((NVTErased, vs2), t2)
+          Just r -> r
+    in Just (v , NVConPat ct dft c cvars)
+splitSingleAltCase _ _ = Nothing
+
+-- | ``harmless'' in the sense of negligible as default expression in |TCase|:
+harmless :: NVTTerm -> Bool
+harmless (NVTError _)  = True
+harmless NVTUnit       = True
+harmless NVTErased     = True
+harmless NVTSort       = True
+harmless _             = False
+
+-- |  @floatPLet@ floats pattern lets occuring in multiple branches
+--    to the least join point of those branches.
+floatPatterns :: TTerm -> TTerm
+floatPatterns = toTTerm' . floatNVPatterns . fromTTerm'
+-- -- For all the details:
+--  floatPatterns t = let
+--    nvt = fromTTerm' t
+--    nvt' = floatNVPatterns nvt
+--    t' = toTTerm' nvt'
+--    in trace (unlines
+--     ["floatPatterns", "    " ++ show nvt, "\n    " ++ show nvt']) t'
+
+floatNVPatterns :: NVTTerm -> TCM NVTTerm
+floatNVPatterns t = execStateT (snd <$> floatPatterns0 t) 0 -- squashPLet [] . snd . floatPatterns0
+
+-- [Floating] only contains maximal elements after unification of patterns
+
+-- | @joinFloatings@ returns the (maximal) common elements first, and then a nubbed concatenation (of the maximal elements).
+joinFloatings :: [Floating] -> [Floating] -> ([Floating], [Floating])
+joinFloatings = h id
+  where
+    h acc [] ps2 = (acc [], ps2)
+    h acc (p1 : ps1) ps2 = let (mc, ps2') = insertFloating p1 ps2
+      in h (acc . maybe id (:) mc) ps1 ps2'
+
+-- If @findFloating fl fls = Just (fl', fls')@
+-- then @fls' = Data.List.delete fl0 fls@ for some @fl0@,
+-- which, unified with @fl@, yields @fl'@.
+findFloating :: Floating -> [Floating] -> Maybe (Floating, [Floating])
+findFloating fl [] = Just (fl, [])
+findFloating plet@(FloatingPLet {pletPat = pat, pletRHS = t}) (fl : fls)
+   | FloatingPLet {pletPat = pat', pletRHS = t'} <- fl
+   , t == t'
+   , Just (pat'', _pu) <- unifyNVPat0 pat pat'
+   = Just (fl { pletPat = pat''}, fls)
+   | otherwise
+   = first (fl :) <$> findFloating plet fls
+findFloating fcase@(FloatingCase cv cpat) (fl : fls)
+  | FloatingCase cv' cpat' <- fl
+  , Just (cpat'', _pu) <- deepUnifyNVConPat (cv, cpat) (cv', cpat')
+  = Just (FloatingCase cv' cpat'')
+  | otherwise = first (fl :) <$> findFloating fcase fls
+
+-- If @insertFloating fl fls = (mfl, fls')@,
+-- then @fls'@ is the result either of adding @fl@ to @fls@,
+--               or of unifying @fl@ with onle of the elements of @fls@,
+-- and @mfl@ is @Just fl'@ if @fl'@ is the result of that unification.
+insertFloating :: Floating -> [Floating] -> (Maybe Floating, [Floating])
+insertFloating fl fls = case findFloating fl fls of
+  Just (fl', fls') -> (Just fl', fl' : fls')
+  Nothing -> (Nothing, fl : fls)
+
+-- | @joinFloatingss@ returns the elements that are ``common'' to
+-- at least two constituent lists first,
+-- and then a nubbed concatenation of all (maximal) elements.
+joinFloatingss :: [[Floating]] -> ([Floating], [Floating])
+joinFloatingss [] = ([], [])
+joinFloatingss [ps] = ([], ps)
+joinFloatingss (ps : pss) = case joinFloatingss pss of
+  (psC, psR) -> case joinFloatings ps psR of
+    (psC', psR') -> (snd (joinFloatings psC' psC), psR')
+
+-- | @floatPatterns0@ duplicates PLet occurrences at join points.
+floatPatterns0 :: NVTTerm -> U TCM ([Floating], NVTTerm)
+floatPatterns0 t = case splitFloating t of
+  Just (fl, t') -> case floatPatterns0 t' of
+    (fls, t'') -> case insertFloating fl fls of
+      (_, fls') -> return (fls', applyFloating fl t'')
+  Nothing -> case t of
+    NVTVar _ -> return ([], t)
+    NVTPrim _ -> return ([], t)
+    NVTDef NVTDefDefault name -> undefined -- ([], t) -- \edcom{WK}{Look up, convert, and float |ccfPLets|!!}
+    NVTDef (NVTDefAbstractPLet _) _ -> return ([], t) -- unlikely to be encountered
+    NVTApp tf tas -> do
+      (psf, tf') <- floatPatterns0 tf
+      (psas, tas') <- unzip <$> map floatPatterns0 tas
+      let (psC, psR) = joinFloatingss $ psf : psas
+      retun (psR, foldr applyFloating (NVTApp tf' tas') psC)
+    NVTLam v tb -> do
+      (ps, tb') <- floatPatterns0 tb
+      return (filter (not . (v `elemVarSet`) . flFreeVars) ps, NVTLam v tb')
+    NVTLit _ -> return ([], t)
+    NVTCon _ -> return ([], t)
+    NVTLet v te tb -> do
+      (psb, tb') <- floatPatterns0 tb
+      let psb' = filter (not . (v `elemVarSet`) . flFreeVars) psb
+      (pse, te') <- floatPatterns0 te
+      case joinFloatings pse psb' of
+          (ps, ps') -> return (ps', foldr applyFloating (NVTLet v te' tb') ps)
+    NVTCase i ct dft alts -> do
+      (psdft, dft') <- floatPatterns0 dft
+      case unzip $ map h alts of
+        (pairs, alts') -> case unzip pairs of
+          (psCs, psRs) -> case joinFloatingss psRs of
+            (psC, psR) -> let psC' = nub $ concat (psCs ++ [psC]) -- \unfinished
+              in (psR, foldr applyFloating (NVTCase i ct dft' alts') psC')
+    NVTUnit -> return ([], t)
+    NVTSort -> return ([], t)
+    NVTErased -> return ([], t)
+    NVTError _ -> return ([], t)
+  where
+    -- |h| returns a pair like |joinFloatings|
+    h :: NVTAlt -> U TCM (([Floating],[Floating]), NVTAlt)
+    h (NVTACon name cvars b) = do
+      (psb, b') <- floatPatterns0 b
+      return (([], filter (\ p -> all (not . (`elemVarSet` flFreeVars p)) cvars) psb), NVTACon name cvars b')
+    h (NVTAGuard g b) = do
+      (psg, g') <- floatPatterns0 g
+      (psb, b') <- floatPatterns0 b
+      return (joinFloatings psg psb, NVTAGuard g' b')
+    h (NVTALit lit b) = do
+      (psb, b') <- floatPatterns0 b
+      return (([], psb), NVTALit lit b')
+
+-- |squashPatterns| is to be called after |floatPatterns0|
+-- \edcomm{WK}{With the improved simplifier, this may not be necessary anymore.}
+{-
+squashPLet :: [PLet] -> NVTTerm -> NVTTerm
+squashPLet ps t = case splitPLet t of
+  Just (p, t') -> case msum $ map (matchPLet p) ps of
+    Just r -> squashPLet ps $ renameNVTTerm r t'
+    Nothing -> applyPLet (pletNVTT p) $ squashPLet (p : ps) t'
+  Nothing -> case t of
+    NVTVar _ -> t
+    NVTPrim _ -> t
+    NVTDef NVTDefDefault name
+      | False -- \edcomm{WK}{Criteria for choosing |dv| and supplying variables!}
+      -> NVTDef (NVTDefAbstractPLet undefined) name
+    NVTDef _ _ -> t
+    NVTApp tf tas -> NVTApp (squashPLet ps tf) (map (squashPLet ps) tas)
+    NVTLam v tb -> NVTLam v $ squashPLet ps tb
+    NVTLit _ -> t
+    NVTCon _ -> t
+    NVTLet v te tb -> NVTLet v (squashPLet ps te) (squashPLet ps tb)
+    NVTCase i ct dft alts -> NVTCase i ct (squashPLet ps dft) (map h alts)
+    NVTUnit -> t
+    NVTSort -> t
+    NVTErased -> t
+    NVTError _ -> t
+  where
+    h :: NVTAlt -> NVTAlt
+    h (NVTACon name cvars b) = NVTACon name cvars $ squashPLet ps b
+    h (NVTAGuard g b) = NVTAGuard (squashPLet ps g) (squashPLet ps b)
+    h (NVTALit lit b) = NVTALit lit $ squashPLet ps b
+-}
+
+-}
